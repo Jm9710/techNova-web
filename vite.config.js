ee60ff3d3f95b21d5bceb415f90cac02e6c8798c
Jorge Morales <jmoralesaltez@gmail.com>
2025-05-21 00:04:44 -0300

    Fix base config for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/techNova-web/', // 👈 Base para GitHub Pages
+  // base: '/techNova-web/', // 👈 Comentar o eliminar para Vercel
 })
